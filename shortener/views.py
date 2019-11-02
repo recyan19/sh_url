32a1467b6c1ffa192c324dbe2642601fdb7fab10
Yan Sokolovskyi <cuex37492@gmail.com>
2019-11-02 21:21:39 +0200

    Create a template for index view

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import render
 
-# Create your views here.
+from shortener.models import Url
+
+
+def index(request):
+    if request.method == "POST":
+        url = request.POST.get('url', '')
+        days = request.POST.get('days', '')
+        url_id = get_short_code_for_url()
+
+        url_object = Url.objects.create(url_id=url_id, url=url, day_to_expiry=days)
+
+        return render(request, 'shortener/index.html', {'url': url_object})
