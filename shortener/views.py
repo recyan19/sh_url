5b4d882397d70f0eed0d4f70d7bf19f5b8f702e2
Yan Sokolovskyi <cuex37492@gmail.com>
2019-11-02 22:17:03 +0200

    Fix index view, add html form to index page

--- shortener/views.py
+++ shortener/views.py
@@ -8,11 +8,12 @@ from shortener.utils import get_short_code_for_url
 def index(request):
     if request.method == "POST":
         url = request.POST.get('url', '')
-        days = request.POST.get('days', '')
+        days = int(request.POST.get('days', ''))
         url_id = get_short_code_for_url()
 
-        url_object = Url.objects.create(url_id=url_id, url=url, day_to_expiry=days)
 
-        return render(request, 'shortener/index.html', {'url': url_object})
+        url_object = Url.objects.create(url_id=url_id, url=url, days_to_expiry=days)
+
+        return render(request, 'shortener/index.html', {'url_object': url_object})
 
     return render(request, 'shortener/index.html')
