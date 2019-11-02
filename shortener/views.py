3fa843f9ff29a4c8f3c69ab8436fdea10b06c5dc
Yan Sokolovskyi <cuex37492@gmail.com>
2019-11-02 21:28:22 +0200

    Implement get_short_code_for_url method

--- shortener/views.py
+++ shortener/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import render
 
 from shortener.models import Url
+from shortener.utils import get_short_code_for_url
 
 
 def index(request):
@@ -12,4 +13,4 @@ def index(request):
 
         url_object = Url.objects.create(url_id=url_id, url=url, day_to_expiry=days)
 
-        return render(request, 'shortener/index.html', {'url': url_object})
+    return render(request, 'shortener/index.html', {'url': url_object})
