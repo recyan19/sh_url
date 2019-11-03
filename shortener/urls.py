51406f95022daa625cdd5a2069f34ba70924df32
Yan Sokolovskyi <cuex37492@gmail.com>
2019-11-03 15:44:06 +0200

    Fix urls order

--- shortener/urls.py
+++ shortener/urls.py
@@ -7,6 +7,6 @@ app_name = 'shortener'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:url_id>/', views.get_original_url, name='get_origin'),
     path('my-urls/', views.user_urls_list, name='user_urls_list'),
+    path('<str:url_id>/', views.get_original_url, name='get_origin'),
 ]
